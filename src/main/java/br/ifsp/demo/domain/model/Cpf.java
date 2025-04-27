993425211b533d42e7cbd868709d3b4f75e44eed
Thiago <47746589+ThiagoDeAM@users.noreply.github.com>
2025-04-27 20:04:39 -0300

    feat: add cpf validation

--- src/main/java/br/ifsp/demo/domain/model/Cpf.java
+++ src/main/java/br/ifsp/demo/domain/model/Cpf.java
@@ -3,14 +3,16 @@ package br.ifsp.demo.domain.model;
 import jakarta.persistence.Embeddable;
 import lombok.Getter;
 
+@Getter
 @Embeddable
 public class Cpf {
-    @Getter
     private String number;
 
     protected Cpf() {}
 
     public Cpf(String number) {
+        if (number == null || !number.matches("\\d{11}"))
+            throw new IllegalArgumentException("Invalid CPF format");
         this.number = number;
     }
 }
