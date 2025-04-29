1bc3e4bba7ba23e0c31f452cd41973f6fbeec1a3
Luiz Henrique do Nascimento Silva <luizhnsc@gmail.com>
2025-04-29 17:08:58 -0300

    test: add unit test for requesting ImmunohematologyExam

--- src/test/java/br/ifsp/demo/application/service/exam/ExamRequestServiceTest.java
+++ src/test/java/br/ifsp/demo/application/service/exam/ExamRequestServiceTest.java
@@ -1,5 +1,35 @@
 package br.ifsp.demo.application.service.exam;
 
-class ExamRequestServiceTest {
+import br.ifsp.demo.domain.model.Appointment;
+import br.ifsp.demo.domain.model.Donation;
+import br.ifsp.demo.domain.model.DonationStatus;
+import br.ifsp.demo.domain.model.Donor;
+import br.ifsp.demo.domain.model.exam.ExamStatus;
+import br.ifsp.demo.domain.model.exam.ImmunohematologyExam;
+import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+
+class ExamRequestServiceTest {
+    @Test
+    void shouldRequestImmunohematologyExamIfDonationIsRegistered(){
+        Donor eligibleDonor = mock(Donor.class);
+        Appointment appointment = mock(Appointment.class);
+        Donation expectedDonation = new Donation(
+                eligibleDonor,
+                appointment,
+                DonationStatus.EM_ANDAMENTO
+        );
+
+        ExamRequestService examRequestService = new ExamRequestService();
+
+        ImmunohematologyExam requestedExam = examRequestService.requestImmunohematologyExam(expectedDonation);
+
+        assertThat(requestedExam.getId()).isNotNull();
+        assertThat(requestedExam.getDonation()).isEqualTo(expectedDonation);
+        assertThat(requestedExam.getStatus()).isEqualTo(ExamStatus.UNDER_ANALYSIS);
+        assertThat(requestedExam.getCreatedAt()).isNotNull();
+        assertThat(requestedExam.getUpdatedAt()).isNotNull();
+    }
 }
