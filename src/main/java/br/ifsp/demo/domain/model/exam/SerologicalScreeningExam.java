e5138f2b6eab779761c97893d6f74b34f10bfee7
Luiz Henrique do Nascimento Silva <luizhnsc@gmail.com>
2025-04-29 20:05:17 -0300

    feat: add @Entity annotation to SerologicalScreeningExam class

--- src/main/java/br/ifsp/demo/domain/model/exam/SerologicalScreeningExam.java
+++ src/main/java/br/ifsp/demo/domain/model/exam/SerologicalScreeningExam.java
@@ -1,7 +1,9 @@
 package br.ifsp.demo.domain.model.exam;
 
 import br.ifsp.demo.domain.model.Donation;
+import jakarta.persistence.Entity;
 
+@Entity
 public class SerologicalScreeningExam extends Exam {
     public SerologicalScreeningExam(Donation donatoin) {
         super(donatoin, ExamStatus.UNDER_ANALYSIS);
