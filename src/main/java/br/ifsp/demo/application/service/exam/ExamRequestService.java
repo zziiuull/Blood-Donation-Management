a3f4b92f36a48acd9465cae171853e3fe78198fb
Luiz Henrique do Nascimento Silva <luizhnsc@gmail.com>
2025-04-29 17:41:57 -0300

    feat: add ExamRepository to ExamRequestService

--- src/main/java/br/ifsp/demo/application/service/exam/ExamRequestService.java
+++ src/main/java/br/ifsp/demo/application/service/exam/ExamRequestService.java
@@ -2,9 +2,19 @@ package br.ifsp.demo.application.service.exam;
 
 import br.ifsp.demo.domain.model.Donation;
 import br.ifsp.demo.domain.model.exam.ImmunohematologyExam;
+import br.ifsp.demo.domain.repository.exam.ExamRepository;
+import org.springframework.stereotype.Service;
 
+@Service
 public class ExamRequestService {
+    private final ExamRepository examRepository;
+
+    public ExamRequestService(ExamRepository examRepository) {
+        this.examRepository = examRepository;
+    }
+
     public ImmunohematologyExam requestImmunohematologyExam(Donation donation){
-        return new ImmunohematologyExam(donation);
+        ImmunohematologyExam immunohematologyExam = new ImmunohematologyExam(donation);
+        return examRepository.save(immunohematologyExam);
     }
 }
