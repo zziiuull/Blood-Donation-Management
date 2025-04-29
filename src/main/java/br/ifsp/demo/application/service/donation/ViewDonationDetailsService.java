e4aaef22794446debb07a63ad237275d7c445486
Luiz Henrique do Nascimento Silva <luizhnsc@gmail.com>
2025-04-29 16:50:53 -0300

    feat: add ExamRepository to ViewDonationDetailsService

--- src/main/java/br/ifsp/demo/application/service/donation/ViewDonationDetailsService.java
+++ src/main/java/br/ifsp/demo/application/service/donation/ViewDonationDetailsService.java
@@ -3,31 +3,35 @@ package br.ifsp.demo.application.service.donation;
 import br.ifsp.demo.application.service.donation.dto.DonationDetailsDTO;
 import br.ifsp.demo.application.service.donation.dto.ExamDTO;
 import br.ifsp.demo.domain.model.Donation;
-import br.ifsp.demo.domain.model.Exam;
+import br.ifsp.demo.domain.model.exam.Exam;
 import br.ifsp.demo.domain.repository.DonationRepository;
+import br.ifsp.demo.domain.repository.exam.ExamRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.UUID;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 @Service
 public class ViewDonationDetailsService {
 
     private final DonationRepository donationRepository;
+    private final ExamRepository examRepository;
 
-    public ViewDonationDetailsService(DonationRepository donationRepository) {
+    public ViewDonationDetailsService(DonationRepository donationRepository, ExamRepository examRepository) {
         this.donationRepository = donationRepository;
+        this.examRepository = examRepository;
     }
 
     public DonationDetailsDTO getDonationDetails(UUID donationId) {
         Donation donation = donationRepository.findById(donationId)
                 .orElseThrow(() -> new IllegalArgumentException("Donation does not exist"));
 
-        List<ExamDTO> exams = donation.getExams().stream()
-                .map(this::toExamDTO)
-                .collect(Collectors.toList());
+        List<ExamDTO> exams = examRepository.findByDonationId(donationId).stream().map(e -> new ExamDTO(
+                e.getId(),
+                e.getStatus(),
+                e.getCreatedAt()
+        )).toList();
 
         return new DonationDetailsDTO(
                 donation.getId(),
@@ -40,7 +44,7 @@ public class ViewDonationDetailsService {
         return new ExamDTO(
                 exam.getId(),
                 exam.getStatus(),
-                exam.getPerformedAt()
+                exam.getCreatedAt()
         );
     }
 }
