3c335e143bb2dd685b47e01e7feab3fac96bd10d
Luiz Henrique do Nascimento Silva <luizhnsc@gmail.com>
2025-04-29 17:31:23 -0300

    test: organize tests for ExamRequestService with nested structure

--- src/test/java/br/ifsp/demo/application/service/exam/ExamRequestServiceTest.java
+++ src/test/java/br/ifsp/demo/application/service/exam/ExamRequestServiceTest.java
@@ -6,30 +6,40 @@ import br.ifsp.demo.domain.model.DonationStatus;
 import br.ifsp.demo.domain.model.Donor;
 import br.ifsp.demo.domain.model.exam.ExamStatus;
 import br.ifsp.demo.domain.model.exam.ImmunohematologyExam;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
 
 class ExamRequestServiceTest {
-    @Test
-    void shouldRequestImmunohematologyExamIfDonationIsRegistered(){
-        Donor eligibleDonor = mock(Donor.class);
-        Appointment appointment = mock(Appointment.class);
-        Donation expectedDonation = new Donation(
-                eligibleDonor,
-                appointment,
-                DonationStatus.EM_ANDAMENTO
-        );
+    @Nested
+    @DisplayName("For valid testes")
+    class ValidTests {
+        @Test
+        @Tag("TDD")
+        @Tag("UnitTest")
+        @DisplayName("Should request immunohematology exam if donation is registered")
+        void shouldRequestImmunohematologyExamIfDonationIsRegistered(){
+            Donor eligibleDonor = mock(Donor.class);
+            Appointment appointment = mock(Appointment.class);
+            Donation expectedDonation = new Donation(
+                    eligibleDonor,
+                    appointment,
+                    DonationStatus.EM_ANDAMENTO
+            );
 
-        ExamRequestService examRequestService = new ExamRequestService();
+            ExamRequestService examRequestService = new ExamRequestService();
 
-        ImmunohematologyExam requestedExam = examRequestService.requestImmunohematologyExam(expectedDonation);
+            ImmunohematologyExam requestedExam = examRequestService.requestImmunohematologyExam(expectedDonation);
 
-        assertThat(requestedExam).isNotNull();
-        assertThat(requestedExam.getDonation()).isEqualTo(expectedDonation);
-        assertThat(requestedExam.getStatus()).isEqualTo(ExamStatus.UNDER_ANALYSIS);
-        assertThat(requestedExam.getCreatedAt()).isNotNull();
-        assertThat(requestedExam.getUpdatedAt()).isNotNull();
+            assertThat(requestedExam).isNotNull();
+            assertThat(requestedExam.getDonation()).isEqualTo(expectedDonation);
+            assertThat(requestedExam.getStatus()).isEqualTo(ExamStatus.UNDER_ANALYSIS);
+            assertThat(requestedExam.getCreatedAt()).isNotNull();
+            assertThat(requestedExam.getUpdatedAt()).isNotNull();
+        }
     }
 }
