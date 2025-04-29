47f8a6f14cab0cabc038432abe033b2e09992790
Luiz Henrique do Nascimento Silva <luizhnsc@gmail.com>
2025-04-29 16:49:05 -0300

    feat: add ExamRepository

---
+++ src/main/java/br/ifsp/demo/domain/repository/exam/ExamRepository.java
@@ -0,0 +1,13 @@
+package br.ifsp.demo.domain.repository.exam;
+
+import br.ifsp.demo.domain.model.exam.Exam;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.UUID;
+
+@Repository
+public interface ExamRepository extends JpaRepository<Exam, UUID> {
+    List<Exam> findByDonationId(UUID donationId);
+}
