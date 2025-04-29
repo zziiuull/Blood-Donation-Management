0318414de36ed88785b5582eb2d9ef5950df9a4f
Luiz Henrique do Nascimento Silva <luizhnsc@gmail.com>
2025-04-29 16:52:15 -0300

    fix: correct import path for ExamStatus in ExamDTO

--- src/main/java/br/ifsp/demo/application/service/donation/dto/ExamDTO.java
+++ src/main/java/br/ifsp/demo/application/service/donation/dto/ExamDTO.java
@@ -1,6 +1,6 @@
 package br.ifsp.demo.application.service.donation.dto;
 
-import br.ifsp.demo.domain.model.ExamStatus;
+import br.ifsp.demo.domain.model.exam.ExamStatus;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
