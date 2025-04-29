51f5a26c8ccd908d5e0f8d690adfda20a303bb30
Luiz Henrique do Nascimento Silva <luizhnsc@gmail.com>
2025-04-29 20:00:37 -0300

    feat: add constructors to SerologicalScreeningExam class

--- src/main/java/br/ifsp/demo/domain/model/exam/SerologicalScreeningExam.java
+++ src/main/java/br/ifsp/demo/domain/model/exam/SerologicalScreeningExam.java
@@ -1,4 +1,13 @@
 package br.ifsp.demo.domain.model.exam;
 
+import br.ifsp.demo.domain.model.Donation;
+
 public class SerologicalScreeningExam extends Exam {
+    public SerologicalScreeningExam(Donation donatoin) {
+        super(donatoin, ExamStatus.UNDER_ANALYSIS);
+    }
+
+    public SerologicalScreeningExam() {
+        super();
+    }
 }
