9ee209f860d8a34fb8d104ef8de5eea1b1499e7e
Luiz Henrique do Nascimento Silva <luizhnsc@gmail.com>
2025-04-30 15:17:55 -0300

    feat: implement ViewExamDetailsService for viewing immunohematology exams

---
+++ src/main/java/br/ifsp/demo/application/service/exam/ViewExamDetailsService.java
@@ -0,0 +1,11 @@
+package br.ifsp.demo.application.service.exam;
+
+import br.ifsp.demo.domain.model.exam.ImmunohematologyExam;
+
+import java.util.UUID;
+
+public class ViewExamDetailsService {
+    public ImmunohematologyExam viewImmunohematologyExam(UUID donationId){
+        return null;
+    }
+}
