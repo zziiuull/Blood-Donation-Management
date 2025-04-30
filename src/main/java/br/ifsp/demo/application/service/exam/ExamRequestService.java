62a453ae6a2cdef0c6480039d7fb38149e918e5e
Luiz Henrique do Nascimento Silva <luizhnsc@gmail.com>
2025-04-29 21:41:46 -0300

    feat: add validation for immunohematology exam request based on donation status

--- src/main/java/br/ifsp/demo/application/service/exam/ExamRequestService.java
+++ src/main/java/br/ifsp/demo/application/service/exam/ExamRequestService.java
@@ -18,6 +18,8 @@ public class ExamRequestService {
 
     public ImmunohematologyExam requestImmunohematologyExam(Donation donation){
         if (donation.getStatus().equals(DonationStatus.APROVADO)) throw new ExamRequestNotAllowedException("Cannot request an immunohematology exam for an approved donation");
+        if (donation.getStatus().equals(DonationStatus.NAO_APROVADO)) throw new ExamRequestNotAllowedException("Cannot request an immunohematology exam for a rejected donation");
+
         ImmunohematologyExam immunohematologyExam = new ImmunohematologyExam(donation);
         return examRepository.save(immunohematologyExam);
     }
