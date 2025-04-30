492f13d9b319a7c978a85a6fed864fa55ae1c18b
Luiz Henrique do Nascimento Silva <luizhnsc@gmail.com>
2025-04-30 15:17:18 -0300

    feat: add bloodType and irregularAntibodies attributes to ImmunohematologyExam

--- src/main/java/br/ifsp/demo/domain/model/exam/ImmunohematologyExam.java
+++ src/main/java/br/ifsp/demo/domain/model/exam/ImmunohematologyExam.java
@@ -1,10 +1,16 @@
 package br.ifsp.demo.domain.model.exam;
 
+import br.ifsp.demo.domain.model.BloodType;
 import br.ifsp.demo.domain.model.Donation;
 import jakarta.persistence.Entity;
+import lombok.Getter;
 
 @Entity
+@Getter
 public class ImmunohematologyExam extends Exam {
+    private BloodType bloodType;
+    private IrregularAntibodies irregularAntibodies;
+
     public ImmunohematologyExam(Donation donatoin) {
         super(donatoin, ExamStatus.UNDER_ANALYSIS);
     }
