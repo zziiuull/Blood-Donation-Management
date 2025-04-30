e7038297ae4040b6b67cd0eee9ba2db285356473
Luiz Henrique do Nascimento Silva <luizhnsc@gmail.com>
2025-04-30 15:13:07 -0300

    test: add unit test for viewing immunohematology exam details

--- src/test/java/br/ifsp/demo/application/service/exam/ViewExamDetailsServiceTest.java
+++ src/test/java/br/ifsp/demo/application/service/exam/ViewExamDetailsServiceTest.java
@@ -15,5 +15,32 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
 
 class ViewExamDetailsServiceTest {
-   
+    private ViewExamDetailsService sut = new ViewExamDetailsService();
+
+    @Test
+    @Tag("TDD")
+    @Tag("UnitTest")
+    @DisplayName("Should view exam details when donation is registered and has an immunohematology exam")
+    void shouldViewExamDetailsWhenDonationIsRegisteredAndHasAnImmunohematologyExam(){
+        Donor eligibleDonor = mock(Donor.class);
+        Appointment appointment = mock(Appointment.class);
+        Donation expectedDonation = new Donation(
+                eligibleDonor,
+                appointment,
+                DonationStatus.EM_ANDAMENTO
+        );
+
+        UUID donationId = UUID.randomUUID();
+
+        ImmunohematologyExam expectedExam = new ImmunohematologyExam(expectedDonation);
+        ImmunohematologyExam result = sut.viewImmunohematologyExam(donationId);
+
+        assertThat(result.getDonation()).isEqualTo(expectedExam.getDonation());
+        assertThat(result.getDonation().getStatus()).isEqualTo(expectedExam.getDonation().getStatus());
+        assertThat(result.getCreatedAt()).isEqualTo(expectedExam.getCreatedAt());
+        assertThat(result.getUpdatedAt()).isEqualTo(expectedExam.getUpdatedAt());
+        assertThat(result.getBloodType()).isEqualTo(expectedExam.getBloodType());
+        assertThat(result.getIrregularAntibodies()).isEqualTo(expectedExam.getIrregularAntibodies());
+        assertThat(result.getObservations()).isEqualTo(expectedExam.getObservations());
+    }
 }
