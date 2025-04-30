1164eee5d865f8c0c4bb45fe57ddbac99a2da78a
Luiz Henrique do Nascimento Silva <luizhnsc@gmail.com>
2025-04-30 15:10:40 -0300

    test: add ViewExamDetailsServiceTest

---
+++ src/test/java/br/ifsp/demo/application/service/exam/ViewExamDetailsServiceTest.java
@@ -0,0 +1,19 @@
+package br.ifsp.demo.application.service.exam;
+
+import br.ifsp.demo.domain.model.Appointment;
+import br.ifsp.demo.domain.model.Donation;
+import br.ifsp.demo.domain.model.DonationStatus;
+import br.ifsp.demo.domain.model.Donor;
+import br.ifsp.demo.domain.model.exam.ImmunohematologyExam;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+
+class ViewExamDetailsServiceTest {
+   
+}
