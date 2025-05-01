1043c73aff3a2542800375e51f5b790cd413d717
Shoji <hshojikato@gmail.com>
2025-05-01 19:19:56 -0300

    feat: verify if exam analysis is contradictory by its fields

--- src/main/java/br/ifsp/demo/application/service/exam/ExamRegistrationService.java
+++ src/main/java/br/ifsp/demo/application/service/exam/ExamRegistrationService.java
@@ -1,11 +1,9 @@
 package br.ifsp.demo.application.service.exam;
 
-import br.ifsp.demo.domain.model.exam.Exam;
-import br.ifsp.demo.domain.model.exam.ExamStatus;
-import br.ifsp.demo.domain.model.exam.ImmunohematologyExam;
-import br.ifsp.demo.domain.model.exam.SerologicalScreeningExam;
+import br.ifsp.demo.domain.model.exam.*;
 import br.ifsp.demo.domain.repository.exam.ExamRepository;
 import br.ifsp.demo.exception.ExamAlreadyAnalyzedException;
+import br.ifsp.demo.exception.InvalidExamAnalysisException;
 import br.ifsp.demo.exception.InvalidUpdatedTimeException;
 import org.springframework.stereotype.Service;
 
@@ -27,17 +25,11 @@ public class ExamRegistrationService {
 
         if (!isInTheFuture(updatedAt)) throw new InvalidUpdatedTimeException("Updated time must be in the future");
 
+        if (!isFieldsValidForApproving(immunohematologyExam)) throw new InvalidExamAnalysisException("Immunohematology exam has invalid field(s) for approving");
+
         approveExam(immunohematologyExam, updatedAt);
 
         return examRepository.save(immunohematologyExam);
-    }
-
-    public SerologicalScreeningExam registerApprovedExam(SerologicalScreeningExam serologicalScreeningExam, LocalDateTime updatedAt) {
-        if (!isInTheFuture(updatedAt)) throw new InvalidUpdatedTimeException("Updated time must be in the future");
-
-        approveExam(serologicalScreeningExam, updatedAt);
-
-        return examRepository.save(serologicalScreeningExam);
     }
 
     private boolean isUnderAnalysis(Exam exam){
@@ -48,9 +40,34 @@ public class ExamRegistrationService {
         return updatedAt != null && (updatedAt.isEqual(LocalDateTime.now()) || updatedAt.isAfter(LocalDateTime.now()));
     }
 
+    private boolean isFieldsValidForApproving(ImmunohematologyExam exam){
+        return exam.getIrregularAntibodies() == IrregularAntibodies.NEGATIVE;
+    }
+
     private void approveExam(Exam exam, LocalDateTime updatedAt) {
         exam.setStatus(ExamStatus.APPROVED);
         exam.setUpdatedAt(updatedAt);
+    }
+
+    public SerologicalScreeningExam registerApprovedExam(SerologicalScreeningExam serologicalScreeningExam, LocalDateTime updatedAt) {
+        if (!isUnderAnalysis(serologicalScreeningExam)) throw new ExamAlreadyAnalyzedException("Can not approve exam already analyzed");
+
+        if (!isInTheFuture(updatedAt)) throw new InvalidUpdatedTimeException("Updated time must be in the future");
+
+        if (!isFieldsValidForApproving(serologicalScreeningExam)) throw new InvalidExamAnalysisException("Serological screening exam has invalid field(s) for approving");
+
+        approveExam(serologicalScreeningExam, updatedAt);
+
+        return examRepository.save(serologicalScreeningExam);
+    }
+
+    private boolean isFieldsValidForApproving(SerologicalScreeningExam exam){
+        if (exam.getHepatitisB() != null && exam.getHepatitisB() == DiseaseDetection.POSITIVE) return false;
+        if (exam.getHepatitisC() != null && exam.getHepatitisC() == DiseaseDetection.POSITIVE) return false;
+        if (exam.getChagasDisease() != null && exam.getChagasDisease() == DiseaseDetection.POSITIVE) return false;
+        if (exam.getSyphilis() != null && exam.getSyphilis() == DiseaseDetection.POSITIVE) return false;
+        if (exam.getAids() != null && exam.getAids() == DiseaseDetection.POSITIVE) return false;
+        return exam.getHtlv1_2() != null && exam.getHtlv1_2() != DiseaseDetection.POSITIVE;
     }
 
     public ImmunohematologyExam registerRejectedExam(ImmunohematologyExam immunohematologyExam, LocalDateTime updatedAt) {
@@ -58,9 +75,20 @@ public class ExamRegistrationService {
 
         if (!isInTheFuture(updatedAt)) throw new InvalidUpdatedTimeException("Updated time must be in the future");
 
+        if (!isFieldsValidForRejecting(immunohematologyExam)) throw new InvalidExamAnalysisException("Immunohematology exam has invalid field(s) for rejecting");
+
         rejectExam(immunohematologyExam, updatedAt);
 
         return examRepository.save(immunohematologyExam);
+    }
+
+    private boolean isFieldsValidForRejecting(ImmunohematologyExam exam){
+        return exam.getIrregularAntibodies() == IrregularAntibodies.POSITIVE;
+    }
+
+    private void rejectExam(Exam exam, LocalDateTime updatedAt) {
+        exam.setStatus(ExamStatus.REJECTED);
+        exam.setUpdatedAt(updatedAt);
     }
 
     public SerologicalScreeningExam registerRejectedExam(SerologicalScreeningExam serologicalScreeningExam, LocalDateTime updatedAt) {
@@ -68,13 +96,19 @@ public class ExamRegistrationService {
 
         if (!isInTheFuture(updatedAt)) throw new InvalidUpdatedTimeException("Updated time must be in the future");
 
+        if (!isFieldsValidForRejecting(serologicalScreeningExam)) throw new InvalidExamAnalysisException("Serological screening exam has invalid field(s) for rejecting");
+
         rejectExam(serologicalScreeningExam, updatedAt);
 
         return examRepository.save(serologicalScreeningExam);
     }
 
-    private void rejectExam(Exam exam, LocalDateTime updatedAt) {
-        exam.setStatus(ExamStatus.REJECTED);
-        exam.setUpdatedAt(updatedAt);
+    private boolean isFieldsValidForRejecting(SerologicalScreeningExam exam){
+        if (exam.getHepatitisB() != null && exam.getHepatitisB() == DiseaseDetection.POSITIVE) return true;
+        if (exam.getHepatitisC() != null && exam.getHepatitisC() == DiseaseDetection.POSITIVE) return true;
+        if (exam.getChagasDisease() != null && exam.getChagasDisease() == DiseaseDetection.POSITIVE) return true;
+        if (exam.getSyphilis() != null && exam.getSyphilis() == DiseaseDetection.POSITIVE) return true;
+        if (exam.getAids() != null && exam.getAids() == DiseaseDetection.POSITIVE) return true;
+        return exam.getHtlv1_2() != null && exam.getHtlv1_2() == DiseaseDetection.POSITIVE;
     }
 }
