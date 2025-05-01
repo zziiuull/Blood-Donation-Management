7a2cebc527ac8cf53b42157e7a1744ea972249c1
Shoji <hshojikato@gmail.com>
2025-05-01 17:32:39 -0300

    feat: verify if exam was already analyzed

--- src/main/java/br/ifsp/demo/application/service/exam/ExamRegistrationService.java
+++ src/main/java/br/ifsp/demo/application/service/exam/ExamRegistrationService.java
@@ -5,6 +5,7 @@ import br.ifsp.demo.domain.model.exam.ExamStatus;
 import br.ifsp.demo.domain.model.exam.ImmunohematologyExam;
 import br.ifsp.demo.domain.model.exam.SerologicalScreeningExam;
 import br.ifsp.demo.domain.repository.exam.ExamRepository;
+import br.ifsp.demo.exception.ExamAlreadyAnalyzedException;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
@@ -21,6 +22,8 @@ public class ExamRegistrationService {
     }
 
     public ImmunohematologyExam registerApprovedExam(ImmunohematologyExam immunohematologyExam, LocalDateTime updatedAt) {
+        if (!isUnderAnalysis(immunohematologyExam)) throw new ExamAlreadyAnalyzedException("Can not approve exam already analyzed");
+
         approveExam(immunohematologyExam, updatedAt);
 
         return examRepository.save(immunohematologyExam);
@@ -32,18 +35,26 @@ public class ExamRegistrationService {
         return examRepository.save(serologicalScreeningExam);
     }
 
+    private boolean isUnderAnalysis(Exam exam){
+        return exam.getStatus() == ExamStatus.UNDER_ANALYSIS;
+    }
+
     private void approveExam(Exam exam, LocalDateTime updatedAt) {
         exam.setStatus(ExamStatus.APPROVED);
         exam.setUpdatedAt(updatedAt);
     }
 
     public ImmunohematologyExam registerRejectedExam(ImmunohematologyExam immunohematologyExam, LocalDateTime updatedAt) {
+        if (!isUnderAnalysis(immunohematologyExam)) throw new ExamAlreadyAnalyzedException("Can not approve exam already analyzed");
+
         rejectExam(immunohematologyExam, updatedAt);
 
         return examRepository.save(immunohematologyExam);
     }
 
     public SerologicalScreeningExam registerRejectedExam(SerologicalScreeningExam serologicalScreeningExam, LocalDateTime updatedAt) {
+        if (!isUnderAnalysis(serologicalScreeningExam)) throw new ExamAlreadyAnalyzedException("Can not approve exam already analyzed");
+
         rejectExam(serologicalScreeningExam, updatedAt);
 
         return examRepository.save(serologicalScreeningExam);
