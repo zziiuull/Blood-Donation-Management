e568541a656183ed505535b3759a1099d6e571d9
Shoji <hshojikato@gmail.com>
2025-05-01 15:37:16 -0300

    feat: implement ExamRegistrationService for registering immunohematology exam

--- src/main/java/br/ifsp/demo/application/service/exam/ExamRegistrationService.java
+++ src/main/java/br/ifsp/demo/application/service/exam/ExamRegistrationService.java
@@ -1,4 +1,27 @@
 package br.ifsp.demo.application.service.exam;
 
+import br.ifsp.demo.domain.model.exam.ExamStatus;
+import br.ifsp.demo.domain.model.exam.ImmunohematologyExam;
+import br.ifsp.demo.domain.repository.exam.ExamRepository;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Service
 public class ExamRegistrationService {
+    private ExamRepository examRepository;
+
+    public ExamRegistrationService() {
+    }
+
+    public ExamRegistrationService(ExamRepository examRepository) {
+        this.examRepository = examRepository;
+    }
+
+    public ImmunohematologyExam register(ImmunohematologyExam immunohematologyExam, LocalDateTime updatedAt) {
+        immunohematologyExam.setStatus(ExamStatus.APPROVED);
+        immunohematologyExam.setUpdatedAt(updatedAt);
+
+        return examRepository.save(immunohematologyExam);
+    }
 }
