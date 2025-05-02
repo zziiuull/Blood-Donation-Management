0d4723579975793e10b47b0fb5f00e6a11026209
Shoji <hshojikato@gmail.com>
2025-05-02 16:04:21 -0300

    feat: add AppointmentRepository dependency and use appointment id for paramenter

--- src/main/java/br/ifsp/demo/controller/donation/RegisterRequest.java
+++ src/main/java/br/ifsp/demo/controller/donation/RegisterRequest.java
@@ -1,9 +1,9 @@
 package br.ifsp.demo.controller.donation;
 
-import java.time.LocalDateTime;
 import java.util.UUID;
 
 public record RegisterRequest (
-        UUID donorId
+        UUID donorId,
+        UUID appointmentId
 ) {
 }
