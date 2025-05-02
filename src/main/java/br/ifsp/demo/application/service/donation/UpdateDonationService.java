470f61e6753c248f2d98792a579c4bf84d558fdc
Shoji <hshojikato@gmail.com>
2025-05-02 07:37:51 -0300

    feat: add Service annotation

--- src/main/java/br/ifsp/demo/application/service/donation/UpdateDonationService.java
+++ src/main/java/br/ifsp/demo/application/service/donation/UpdateDonationService.java
@@ -4,13 +4,18 @@ import br.ifsp.demo.domain.model.Donation;
 import br.ifsp.demo.domain.model.DonationStatus;
 import br.ifsp.demo.domain.repository.donation.DonationRepository;
 import br.ifsp.demo.exception.EntityNotFoundException;
+import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 import java.util.Optional;
 import java.util.UUID;
 
+@Service
 public class UpdateDonationService {
     private DonationRepository donationRepository;
+
+    public UpdateDonationService() {
+    }
 
     public UpdateDonationService(DonationRepository donationRepository) {
         this.donationRepository = donationRepository;
