dfc8deb82a91c802c3411fe4547de51762fafb30
Luiz Henrique do Nascimento Silva <luizhnsc@gmail.com>
2025-05-02 15:04:36 -0300

    feat: add fromRequest method to ImmunohematologyExamDTO

--- src/main/java/br/ifsp/demo/application/service/exam/dto/ImmunohematologyExamDTO.java
+++ src/main/java/br/ifsp/demo/application/service/exam/dto/ImmunohematologyExamDTO.java
@@ -1,5 +1,6 @@
 package br.ifsp.demo.application.service.exam.dto;
 
+import br.ifsp.demo.controller.exam.request.ImmunohematologyExamRequest;
 import br.ifsp.demo.domain.model.common.BloodType;
 import br.ifsp.demo.domain.model.exam.IrregularAntibodies;
 
@@ -7,4 +8,11 @@ public record ImmunohematologyExamDTO (
         BloodType bloodType,
         IrregularAntibodies irregularAntibodies
 ) {
+
+    public static ImmunohematologyExamDTO fromRequest(ImmunohematologyExamRequest exam){
+        return new ImmunohematologyExamDTO(
+                exam.bloodType(),
+                exam.irregularAntibodies()
+        );
+    }
 }
