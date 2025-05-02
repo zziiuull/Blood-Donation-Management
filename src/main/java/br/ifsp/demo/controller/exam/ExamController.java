fe9d8fe773e01af80c19495db439c2d3ec232b5e
Luiz Henrique do Nascimento Silva <luizhnsc@gmail.com>
2025-05-02 16:06:55 -0300

    feat: add approveImmunohematologyExam method

--- src/main/java/br/ifsp/demo/controller/exam/ExamController.java
+++ src/main/java/br/ifsp/demo/controller/exam/ExamController.java
@@ -1,6 +1,8 @@
 package br.ifsp.demo.controller.exam;
-
+import br.ifsp.demo.application.service.exam.ExamRegistrationService;
 import br.ifsp.demo.application.service.exam.ExamRequestService;
+import br.ifsp.demo.application.service.exam.dto.ImmunohematologyExamDTO;
+import br.ifsp.demo.controller.exam.request.ImmunohematologyExamRequest;
 import br.ifsp.demo.controller.exam.response.ImmunohematologyExamResponse;
 import br.ifsp.demo.controller.exam.response.SerologicalScreeningExamResponse;
 import br.ifsp.demo.domain.model.donation.Donation;
@@ -8,13 +10,14 @@ import br.ifsp.demo.domain.model.exam.ImmunohematologyExam;
 import br.ifsp.demo.domain.model.exam.SerologicalScreeningExam;
 import br.ifsp.demo.domain.repository.donation.DonationRepository;
 import br.ifsp.demo.exception.EntityNotFoundException;
+import br.ifsp.demo.security.auth.AuthenticationInfoService;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.*;
+
 import java.util.UUID;
 
 @RestController
@@ -23,6 +26,7 @@ import java.util.UUID;
 @Tag(name = "Exam controller")
 public class ExamController {
     private final ExamRequestService examRequestService;
+    private final ExamRegistrationService examRegistrationService;
     private final DonationRepository donationRepository;
 
     @PostMapping("request/immunohematology/{donationId}")
@@ -36,6 +40,7 @@ public class ExamController {
         return ResponseEntity.ok(new ImmunohematologyExamResponse(immunohematologyExam));
     }
 
+
     @PostMapping("request/serologicalscreening/{donationId}")
     public ResponseEntity<SerologicalScreeningExamResponse> requestSerologicalScreeningExam(
             @PathVariable UUID donationId){
@@ -45,5 +50,12 @@ public class ExamController {
         SerologicalScreeningExam serologicalScreeningExam = examRequestService.requestSerologicalScreeningExam(donation);
 
         return ResponseEntity.ok(new SerologicalScreeningExamResponse(serologicalScreeningExam));
+    }
+
+    @PostMapping("/register/immunohematology/approve/{examId}")
+    public ResponseEntity<ImmunohematologyExam> approveImmunohematologyExam(
+            @PathVariable UUID examId,
+            @RequestBody @Valid ImmunohematologyExamRequest exam) {
+        return ResponseEntity.ok(examRegistrationService.registerApprovedExam(examId, ImmunohematologyExamDTO.fromRequest(exam)));
     }
 }
