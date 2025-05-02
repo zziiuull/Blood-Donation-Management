32871de72a8258649d07349ae592ff10cdffd378
Luiz Henrique do Nascimento Silva <luizhnsc@gmail.com>
2025-05-02 16:13:15 -0300

    feat: add fromRequest method to SerologicalScreeningExamDTO

--- src/main/java/br/ifsp/demo/application/service/exam/dto/SerologicalScreeningExamDTO.java
+++ src/main/java/br/ifsp/demo/application/service/exam/dto/SerologicalScreeningExamDTO.java
@@ -1,5 +1,7 @@
 package br.ifsp.demo.application.service.exam.dto;
 
+import br.ifsp.demo.controller.exam.request.ImmunohematologyExamRequest;
+import br.ifsp.demo.controller.exam.request.SerologicalScreeningExamRequest;
 import br.ifsp.demo.domain.model.exam.DiseaseDetection;
 
 import java.time.LocalDateTime;
@@ -12,4 +14,14 @@ public record SerologicalScreeningExamDTO(
         DiseaseDetection aids,
         DiseaseDetection htlv1_2
 ) {
+    public static SerologicalScreeningExamDTO fromRequest(SerologicalScreeningExamRequest exam){
+        return new SerologicalScreeningExamDTO(
+                exam.hepatitisB(),
+                exam.hepatitisC(),
+                exam.chagasDisease(),
+                exam.syphilis(),
+                exam.aids(),
+                exam.htlv1_2()
+        );
+    }
 }
