58c4eca7106f1f4a1d8960935e18b69e7d07cb05
Thiago <47746589+ThiagoDeAM@users.noreply.github.com>
2025-05-03 18:47:49 -0300

    feat: add method in DonationRepository

--- src/main/java/br/ifsp/demo/domain/repository/donation/DonationRepository.java
+++ src/main/java/br/ifsp/demo/domain/repository/donation/DonationRepository.java
@@ -1,6 +1,8 @@
 package br.ifsp.demo.domain.repository.donation;
 
+import br.ifsp.demo.domain.model.donation.Appointment;
 import br.ifsp.demo.domain.model.donation.Donation;
+import br.ifsp.demo.domain.model.donor.Donor;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +10,5 @@ import java.util.UUID;
 
 @Repository
 public interface DonationRepository extends JpaRepository<Donation, UUID> {
+    boolean existsByDonorAndAppointment(Donor donor, Appointment appointment);
 }
