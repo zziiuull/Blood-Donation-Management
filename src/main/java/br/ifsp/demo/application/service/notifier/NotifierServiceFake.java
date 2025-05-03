59d16c79f9fdd21c8366640400732ad2c41a0130
Thiago <47746589+ThiagoDeAM@users.noreply.github.com>
2025-05-03 19:57:03 -0300

    refactor: change sout to logger in NotifierServiceFake

--- src/main/java/br/ifsp/demo/application/service/notifier/NotifierServiceFake.java
+++ src/main/java/br/ifsp/demo/application/service/notifier/NotifierServiceFake.java
@@ -1,12 +1,17 @@
 package br.ifsp.demo.application.service.notifier;
 
 import br.ifsp.demo.domain.model.donor.Donor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+
 
 @Service
 public class NotifierServiceFake implements NotifierService {
+    private static final Logger LOGGER =  LoggerFactory.getLogger(NotifierServiceFake.class);
+
     @Override
     public void notify(Donor donor, String msg) {
-        System.out.println(donor.getName() + " " + msg);
+        LOGGER.info("{} {}", donor.getName(), msg);
     }
 }
