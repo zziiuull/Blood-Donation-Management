57ee4a180a7d0c8a59f994a2165a016e390d61e4
Luiz Henrique do Nascimento Silva <luizhnsc@gmail.com>
2025-05-05 17:43:55 -0300

    feat: add Crm and State classes for Physician

---
+++ src/main/java/br/ifsp/demo/domain/model/physician/Crm.java
@@ -0,0 +1,20 @@
+package br.ifsp.demo.domain.model.physician;
+
+import jakarta.persistence.Embeddable;
+import lombok.Getter;
+import java.io.Serializable;
+
+@Getter
+@Embeddable
+public class Crm implements Serializable {
+    String value;
+    State state;
+
+    public Crm() {
+    }
+
+    public Crm(String value, State state) {
+        this.value = value;
+        this.state = state;
+    }
+}
