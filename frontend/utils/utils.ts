dad7d67d9a3505263cc5619163c5d9466bade493
Luiz Henrique do Nascimento Silva <luizhnsc@gmail.com>
2025-05-26 16:22:19 -0300

    feat: add utility

---
+++ frontend/utils/utils.ts
@@ -0,0 +1,34 @@
+import { BloodType } from "@/types";
+
+export const formatDateTime = (isoString: string) => {
+  const date = new Date(isoString);
+
+  return date.toLocaleString("pt-BR", {
+    day: "2-digit",
+    month: "2-digit",
+    year: "numeric",
+    hour: "2-digit",
+    minute: "2-digit",
+  });
+};
+
+export const formatDate = (isoString: string) => {
+  const date = new Date(isoString);
+
+  return date.toLocaleString("pt-BR", {
+    day: "2-digit",
+    month: "2-digit",
+    year: "numeric",
+  });
+};
+
+export const bloodTypeMap: Map<BloodType, string> = new Map([
+  [BloodType.A_POS, "A+"],
+  [BloodType.A_NEG, "A-"],
+  [BloodType.B_POS, "B+"],
+  [BloodType.B_NEG, "B-"],
+  [BloodType.AB_POS, "AB+"],
+  [BloodType.AB_NEG, "AB-"],
+  [BloodType.O_POS, "O+"],
+  [BloodType.O_NEG, "O-"],
+]);
