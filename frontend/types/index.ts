5baf8ce0e72b2c52f8d3bb704db5b32d9a540bc7
Luiz Henrique do Nascimento Silva <luizhnsc@gmail.com>
2025-05-26 16:21:59 -0300

    feat: add domain types

--- frontend/types/index.ts
+++ frontend/types/index.ts
@@ -3,3 +3,128 @@ import { SVGProps } from "react";
 export type IconSvgProps = SVGProps<SVGSVGElement> & {
   size?: number;
 };
+
+type ContactInfo = {
+  email: string;
+  phone: string;
+  address: string;
+};
+
+export enum Sex {
+  MALE = "MALE",
+  FEMALE = "FEMALE",
+}
+
+export enum BloodType {
+  A_POS = "A_POS",
+  A_NEG = "A_NEG",
+  B_POS = "B_POS",
+  B_NEG = "B_NEG",
+  AB_POS = "AB_POS",
+  AB_NEG = "AB_NEG",
+  O_POS = "O_POS",
+  O_NEG = "O_NEG",
+}
+
+export type Donor = {
+  id: string;
+  name: string;
+  contactInfo: ContactInfo;
+  cpf: string;
+  weight: number;
+  birthDate: string;
+  sex: Sex;
+  bloodType: BloodType;
+};
+
+export type DonorSearchCardProps = {
+  donor: Donor;
+  handleDonorSelect: (Donor: Donor) => void;
+};
+
+export type DonorSearchListContainerProps = {
+  searchValue: string;
+  handleDonorSelect: (Donor: Donor) => void;
+};
+
+export enum AppointmentStatus {
+  SCHEDULED = "SCHEDULED",
+  CANCELED = "CANCELED",
+  COMPLETE = "COMPLETED",
+}
+
+type CollectionSite = {
+  id: string;
+  name: string;
+};
+
+export type Appointment = {
+  id: string;
+  appointmentDate: string;
+  status: AppointmentStatus;
+  collectionSite: CollectionSite;
+  notes: string;
+};
+
+export type DonorDetailsProps = {
+  donor: Donor;
+};
+
+export enum DonationStatus {
+  APPROVED = "APPROVED",
+  REJECTED = "REJECTED",
+  UNDER_ANALYSIS = "UNDER_ANALYSIS",
+}
+
+export type Donation = {
+  id: string;
+  donor: Donor;
+  appointment: Appointment;
+  donationStatus: DonationStatus;
+  createdAt: string;
+  updatedAt: string;
+  notes: string;
+};
+
+export enum ExamStatus {
+  APPROVED = "APPROVED",
+  REJECTED = "REJECTED",
+  UNDER_ANALYSISCANCELED = "UNDER_ANALYSIS",
+}
+export type Exam = {
+  id: string;
+  name?: string;
+  donation: Donation;
+  status: ExamStatus;
+  createdAt: string;
+  updatedAt: string;
+  observations?: string;
+};
+
+export type ExamCardProps = {
+  exam: Exam;
+};
+
+export enum IrregularAntibodies {
+  POSITIVE = "POSTIVE",
+  NEGATIVE = "NEGATIVE",
+}
+
+export enum DiseaseDetection {
+  POSITIVE = "POSTIVE",
+  NEGATIVE = "NEGATIVE",
+}
+
+export type ImmunohemalogyExam = Exam & {
+  bloodType: BloodType;
+  irregularAntibodies: IrregularAntibodies;
+};
+
+export type SerologicalScreeningExam = Exam & {
+  hepatitisB: DiseaseDetection;
+  hepatitisC: DiseaseDetection;
+  chagasDisease: DiseaseDetection;
+  syphilis: DiseaseDetection;
+  aids: DiseaseDetection;
+  htlv1_2: DiseaseDetection;
+};
