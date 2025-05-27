f3f2375dbba442db1867a7b0cdc97b3c12041f44
Thiago <47746589+ThiagoDeAM@users.noreply.github.com>
2025-05-27 18:41:13 -0300

    feat: add exam.ts to export enums

---
+++ frontend/types/exam.ts
@@ -0,0 +1,19 @@
+export type BloodType =
+  | 'A_POS'
+  | 'A_NEG'
+  | 'B_POS'
+  | 'B_NEG'
+  | 'O_POS'
+  | 'O_NEG'
+  | 'AB_POS'
+  | 'AB_NEG';
+
+export type IrregularAntibodies = 'POSITIVE' | 'NEGATIVE';
+
+export type DiseaseDetection = 'POSITIVE' | 'NEGATIVE';
+
+export enum ExamStatus {
+  UNDER_ANALYSIS = 'UNDER_ANALYSIS',
+  APPROVED = 'APPROVED',
+  REJECTED = 'REJECTED'
+}
