a63edacf6cb449ab21d3b7d6934365aaabb014ad
Thiago <47746589+ThiagoDeAM@users.noreply.github.com>
2025-05-27 19:12:09 -0300

    refactor: update exam.ts

--- frontend/types/exam.ts
+++ frontend/types/exam.ts
@@ -1,12 +1,8 @@
 export type BloodType =
-  | 'A_POS'
-  | 'A_NEG'
-  | 'B_POS'
-  | 'B_NEG'
-  | 'O_POS'
-  | 'O_NEG'
-  | 'AB_POS'
-  | 'AB_NEG';
+  | 'A_POS' | 'A_NEG'
+  | 'B_POS' | 'B_NEG'
+  | 'O_POS' | 'O_NEG'
+  | 'AB_POS' | 'AB_NEG';
 
 export type IrregularAntibodies = 'POSITIVE' | 'NEGATIVE';
 
