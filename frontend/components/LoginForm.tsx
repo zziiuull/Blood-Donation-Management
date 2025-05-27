f635a31a4a7efea2dfa1c486460deb8fcc3692d9
Shoji <hshojikato@gmail.com>
2025-05-27 16:09:41 -0300

    refactor: create Login and Register forms components

---
+++ frontend/components/LoginForm.tsx
@@ -0,0 +1,68 @@
+"use client";
+
+import { Button, Input, Form } from "@heroui/react";
+import { useState } from "react";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
+
+import FormContainer from "./FormContainer";
+
+export default function LoginForm() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [isPasswordVisible, setIsPasswordVisible] = useState(false);
+  const togglePasswordVisibility = () =>
+    setIsPasswordVisible(!isPasswordVisible);
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+  }
+
+  return (
+    <FormContainer>
+      <h1 className="text-3xl text-center">Login</h1>
+      <Form className="flex flex-col gap-3" onSubmit={handleSubmit}>
+        <Input
+          isRequired
+          label="Email"
+          placeholder="Enter your email"
+          size="lg"
+          type="email"
+          value={email}
+          variant="bordered"
+          onValueChange={setEmail}
+        />
+        <Input
+          isRequired
+          endContent={
+            <button
+              aria-label="toggle password visibility"
+              className="focus:outline-none"
+              type="button"
+              onClick={togglePasswordVisibility}
+            >
+              {isPasswordVisible ? (
+                <FaEye className="text-2xl text-default-400 pointer-events-none" />
+              ) : (
+                <FaEyeSlash className="text-2xl text-default-400 pointer-events-none" />
+              )}
+            </button>
+          }
+          label="Password"
+          placeholder="Enter your password"
+          size="lg"
+          type={isPasswordVisible ? "text" : "password"}
+          value={password}
+          variant="bordered"
+          onValueChange={setPassword}
+        />
+        <Button
+          className="w-full text-xl py-6 rounded-full"
+          color="primary"
+          type="submit"
+        >
+          Login
+        </Button>
+      </Form>
+    </FormContainer>
+  );
+}
