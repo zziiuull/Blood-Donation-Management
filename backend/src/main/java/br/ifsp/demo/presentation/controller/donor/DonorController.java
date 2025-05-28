167e49cbd079738c1828ef55539f0844fc1ac2c9
Luiz Henrique do Nascimento Silva <luizhnsc@gmail.com>
2025-05-28 14:18:03 -0300

    feat: add DonorController

---
+++ backend/src/main/java/br/ifsp/demo/presentation/controller/donor/DonorController.java
@@ -0,0 +1,54 @@
+package br.ifsp.demo.presentation.controller.donor;
+
+import br.ifsp.demo.application.service.donation.DonationRegisterService;
+import br.ifsp.demo.application.service.donor.DonorService;
+import br.ifsp.demo.application.service.dto.donation.DonationDetailsDTO;
+import br.ifsp.demo.application.service.dto.donor.DonorDTO;
+import br.ifsp.demo.domain.model.donor.Donor;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping(path = "/api/v1/donor")
+@AllArgsConstructor
+@Tag(name = "Donor API")
+public class DonorController {
+    private final DonorService donorService;
+
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200", description = "Successful operation.",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = DonationRegisterService.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "401",
+                    description = "Authentication fails.",
+                    content = @Content(schema = @Schema(hidden = true))
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Donors do not exist.",
+                    content = @Content(schema = @Schema(hidden = true))
+            )
+    })
+    @GetMapping
+    public ResponseEntity<List<DonorDTO>> view() {
+        List<Donor> donors = donorService.getAll();
+        return ResponseEntity.ok(donors.stream().map(DonorDTO::new).toList());
+    }
+}
