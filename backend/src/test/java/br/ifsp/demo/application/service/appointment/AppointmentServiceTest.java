13633c9ee2ee992f660c4aa0a83715dab3ad809f
Luiz Henrique do Nascimento Silva <luizhnsc@gmail.com>
2025-05-28 14:24:52 -0300

    test: add AppointmentServiceTest

---
+++ backend/src/test/java/br/ifsp/demo/application/service/appointment/AppointmentServiceTest.java
@@ -0,0 +1,69 @@
+package br.ifsp.demo.application.service.appointment;
+
+import br.ifsp.demo.application.service.donor.DonorService;
+import br.ifsp.demo.domain.model.common.BloodType;
+import br.ifsp.demo.domain.model.common.ContactInfo;
+import br.ifsp.demo.domain.model.common.Cpf;
+import br.ifsp.demo.domain.model.donation.Appointment;
+import br.ifsp.demo.domain.model.donation.AppointmentStatus;
+import br.ifsp.demo.domain.model.donation.CollectionSite;
+import br.ifsp.demo.domain.model.donor.Donor;
+import br.ifsp.demo.domain.model.donor.Sex;
+import br.ifsp.demo.infrastructure.repository.appointment.AppointmentRepository;
+import br.ifsp.demo.infrastructure.repository.donor.DonorRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class AppointmentServiceTest {
+    @Mock
+    private AppointmentRepository appointmentRepository;
+
+    @InjectMocks
+    private AppointmentService appointmentService;
+
+    @Nested
+    @DisplayName("For valid tests")
+    class ValidTests {
+        @Test
+        @Tag("Functional")
+        @Tag("TDD")
+        @Tag("UnitTest")
+        @DisplayName("Should get all appointments")
+        void shouldGetAllAppointments() {
+            CollectionSite site = new CollectionSite(
+                    "Banco de Doação de Sorocaba",
+                    new ContactInfo("doesangue.sorocaba@email.com", "1533761530", "Av. Anhanguera, n. 715, Sorocaba/SP")
+            );
+            Appointment appointment = new Appointment(
+                    LocalDateTime.now().plusDays(1),
+                    AppointmentStatus.SCHEDULED,
+                    site,
+                    "Primeira doação"
+            );
+            List<Appointment> appointments = List.of(appointment);
+
+            when(appointmentRepository.findAll()).thenReturn(appointments);
+
+            List<Donor> result = appointmentService.getAll();
+
+            assertThat(result).isNotNull();
+            assertThat(result).isEqualTo(appointments);
+
+            verify(appointmentRepository, times(1)).findAll();
+        }
+    }
+}
