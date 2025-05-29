090ff735cd2a2fd4cd0afe6735ceba2ee1c7141b
Thiago <47746589+ThiagoDeAM@users.noreply.github.com>
2025-05-29 17:45:22 -0300

    style: Remove API_URL commented

--- frontend/services/api.ts
+++ frontend/services/api.ts
@@ -1,7 +1,5 @@
 import axios from "./axios";
 
-//export const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8080/api/v1';
-
 export const API_URL = '/api/v1';
 
 const getAuthHeaders = () => {
