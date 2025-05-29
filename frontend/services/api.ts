c776433d54097ac9129b6f4996a1718d4f63c1c8
Luiz Henrique do Nascimento Silva <luizhnsc@gmail.com>
2025-05-29 20:08:04 -0300

    fix: correct request endpoint

--- frontend/services/api.ts
+++ frontend/services/api.ts
@@ -1,34 +1,57 @@
 import axios from "./axios";
 
-export const API_URL = '/api/v1';
+export const API_URL = "/api/v1";
 
 const getAuthHeaders = () => {
-  const token = localStorage.getItem('token');
+  const token = localStorage.getItem("token");
+
   return {
     headers: {
-      Authorization: `Bearer ${token}`
-    }
+      Authorization: `Bearer ${token}`,
+    },
   };
 };
 
 export async function getImmunoExamByDonationId(donationId: string) {
-  const res = await axios.get(`${API_URL}/exam/view/immunohematology/${donationId}`, getAuthHeaders());
+  const res = await axios.get(
+    `${API_URL}/exam/view/immunohematology/${donationId}`,
+    getAuthHeaders(),
+  );
+
   return res.data;
 }
 
-export async function updateImmunoExam(examId: string, data: any, approve = true) {
-  const endpoint = approve ? 'approve' : 'reject';
-  const res = await axios.post(`${API_URL}/exam/immunohematology/${examId}/${endpoint}`, data, getAuthHeaders());
+export async function updateImmunoExam(exam: any, data: any, approve = true) {
+  const endpoint = approve ? "approve" : "reject";
+  const res = await axios.post(
+    `${API_URL}/exam/register/donation/${exam.donationId}/immunohematology/${endpoint}/${exam.id}`,
+    data,
+    getAuthHeaders(),
+  );
+
   return res.data;
 }
 
 export async function getSerologicalExamByDonationId(donationId: string) {
-  const res = await axios.get(`${API_URL}/exam/view/serologicalscreening/${donationId}`, getAuthHeaders());
+  const res = await axios.get(
+    `${API_URL}/exam/view/serologicalscreening/${donationId}`,
+    getAuthHeaders(),
+  );
+
   return res.data;
 }
 
-export async function updateSerologicalExam(examId: string, data: any, approve = true) {
-  const endpoint = approve ? 'approve' : 'reject';
-  const res = await axios.post(`${API_URL}/exam/serologicalscreening/${examId}/${endpoint}`, data, getAuthHeaders());
+export async function updateSerologicalExam(
+  exam: any,
+  data: any,
+  approve = true,
+) {
+  const endpoint = approve ? "approve" : "reject";
+  const res = await axios.post(
+    `${API_URL}/exam/register/donation/${exam.donationId}/serologicalscreening/${endpoint}/${exam.id}`,
+    data,
+    getAuthHeaders(),
+  );
+
   return res.data;
 }
