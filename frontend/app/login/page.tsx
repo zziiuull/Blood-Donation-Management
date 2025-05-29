f53aa56d709221461c8183fb88b567ffc1d60251
Shoji <hshojikato@gmail.com>
2025-05-29 16:26:03 -0300

    feat: redirect logged in users from login and register pages to donation page

--- frontend/app/login/page.tsx
+++ frontend/app/login/page.tsx
@@ -1,8 +1,20 @@
+"use client";
+
 import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 import LoginForm from "./components/LoginForm";
 
 export default function LoginPage() {
+  const router = useRouter();
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+
+    if (token) router.push("/donation");
+  });
+
   return (
     <div className="flex flex-col items-center mt-10 p-4">
       <LoginForm />
--- frontend/app/register/page.tsx
+++ frontend/app/register/page.tsx
@@ -1,8 +1,20 @@
+"use client";
+
 import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 import RegisterForm from "./components/RegisterForm";
 
 export default function RegisterPage() {
+  const router = useRouter();
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+
+    if (token) router.push("/donation");
+  });
+
   return (
     <div className="flex flex-col items-center p-4">
       <RegisterForm />
