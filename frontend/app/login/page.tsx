3f11ca594e7a66e87c9c070501679d36d7222b18
Shoji <hshojikato@gmail.com>
2025-05-29 15:30:08 -0300

    fix: reduce page height

--- frontend/app/login/page.tsx
+++ frontend/app/login/page.tsx
@@ -4,11 +4,11 @@ import LoginForm from "./components/LoginForm";
 
 export default function LoginPage() {
   return (
-    <main className="flex flex-col items-center justify-center min-h-screen p-4">
+    <div className="flex flex-col items-center mt-10 p-4">
       <LoginForm />
       <Link className="mt-5 underline text-slate-500" href="/register">
         Register account
       </Link>
-    </main>
+    </div>
   );
 }
--- frontend/app/register/page.tsx
+++ frontend/app/register/page.tsx
@@ -4,11 +4,11 @@ import RegisterForm from "./components/RegisterForm";
 
 export default function RegisterPage() {
   return (
-    <main className="flex flex-col items-center justify-center min-h-screen p-4">
+    <div className="flex flex-col items-center p-4">
       <RegisterForm />
-      <Link className="mt-5 underline text-slate-500" href="/register">
+      <Link className="mt-5 underline text-slate-500" href="/login">
         Login
       </Link>
-    </main>
+    </div>
   );
 }
