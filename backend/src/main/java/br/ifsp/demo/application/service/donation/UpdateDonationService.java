663bb83d19f30eaa2e6ba3531ea7f0ee60403135
Shoji <hshojikato@gmail.com>
2025-05-30 14:10:00 -0300

    feat: throw if donation is already analyzed

--- backend/src/main/java/br/ifsp/demo/application/service/donation/UpdateDonationService.java
+++ backend/src/main/java/br/ifsp/demo/application/service/donation/UpdateDonationService.java
@@ -3,13 +3,10 @@ package br.ifsp.demo.application.service.donation;
 import br.ifsp.demo.application.service.notifier.NotifierService;
 import br.ifsp.demo.domain.model.donation.Donation;
 import br.ifsp.demo.domain.model.donation.DonationStatus;
-import br.ifsp.demo.domain.model.exam.Exam;
-import br.ifsp.demo.domain.model.exam.ExamStatus;
 import br.ifsp.demo.domain.model.exam.ImmunohematologyExam;
 import br.ifsp.demo.domain.model.exam.SerologicalScreeningExam;
 import br.ifsp.demo.infrastructure.repository.donation.DonationRepository;
 import br.ifsp.demo.infrastructure.repository.exam.ExamRepository;
-import br.ifsp.demo.presentation.exception.CannotFinishDonationWithExamUnderAnalysisException;
 import br.ifsp.demo.presentation.exception.DonationNotFoundException;
 import br.ifsp.demo.presentation.exception.ExamNotFoundException;
 import br.ifsp.demo.presentation.exception.InvalidDonationAnalysisException;
@@ -33,9 +30,9 @@ public class UpdateDonationService {
     }
 
     public Donation approve(UUID donationId, LocalDateTime updatedAt){
-        Optional<Donation> optionalDonation = donationRepository.findById(donationId);
-        if (optionalDonation.isEmpty()) throw new DonationNotFoundException("Donation not found");
-        Donation donation = optionalDonation.get();
+        Donation donation = retrieveDonationById(donationId);
+        if (donation.getStatus() != DonationStatus.UNDER_ANALYSIS)
+            throw new InvalidDonationAnalysisException("Donation status is not under analysis");
 
         ImmunohematologyExam immunohematologyExam = retrieveImmunohematologyExam(donationId);
         SerologicalScreeningExam serologicalScreeningExam = retrieveSerologicalScreeningExam(donationId);
@@ -48,6 +45,12 @@ public class UpdateDonationService {
         notifierService.notify(donation.getDonor(), "Donation approved");
 
         return saved;
+    }
+
+    private Donation retrieveDonationById(UUID donationId) {
+        Optional<Donation> optionalDonation = donationRepository.findById(donationId);
+        if (optionalDonation.isEmpty()) throw new DonationNotFoundException("Donation not found");
+        return optionalDonation.get();
     }
 
     private ImmunohematologyExam retrieveImmunohematologyExam(UUID donationId) {
@@ -68,9 +71,9 @@ public class UpdateDonationService {
     }
 
     public Donation reject(UUID donationId, LocalDateTime updatedAt){
-        Optional<Donation> optionalDonation = donationRepository.findById(donationId);
-        if (optionalDonation.isEmpty()) throw new DonationNotFoundException("Donation not found");
-        Donation donation = optionalDonation.get();
+        Donation donation = retrieveDonationById(donationId);
+        if (donation.getStatus() != DonationStatus.UNDER_ANALYSIS)
+            throw new InvalidDonationAnalysisException("Donation status is not under analysis");
 
         ImmunohematologyExam immunohematologyExam = retrieveImmunohematologyExam(donationId);
         SerologicalScreeningExam serologicalScreeningExam = retrieveSerologicalScreeningExam(donationId);
