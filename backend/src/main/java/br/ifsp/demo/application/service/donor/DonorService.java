dd516dc44e54e88af9ab9638d3d0b3733293ceb1
Thiago <47746589+ThiagoDeAM@users.noreply.github.com>
2025-06-02 20:57:13 -0300

    feat: add canDonateToday method in DonorService

--- backend/src/main/java/br/ifsp/demo/application/service/donor/DonorService.java
+++ backend/src/main/java/br/ifsp/demo/application/service/donor/DonorService.java
@@ -4,6 +4,7 @@ import br.ifsp.demo.domain.model.donor.Donor;
 import br.ifsp.demo.infrastructure.repository.donor.DonorRepository;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Service
@@ -16,5 +17,15 @@ public class DonorService {
 
     public List<Donor> getAll(){
         return donorRepository.findAll();
+    }
+
+    public boolean canDonateToday(Donor donor, LocalDate today, LocalDate lastDonationDate) {
+        if (donor == null || today == null)
+            throw new IllegalArgumentException("Arguments must not be null");
+
+        if (!donor.isEligibleForDonation())
+            return false;
+
+        return lastDonationDate == null || !lastDonationDate.isEqual(today);
     }
 }
