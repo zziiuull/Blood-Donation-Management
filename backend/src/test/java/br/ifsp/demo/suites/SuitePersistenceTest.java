f7b83d5c063e884bfb6029e0735658910021b46e
Felipe Santos Lourenço <127883811+fezinnn@users.noreply.github.com>
2025-06-11 17:24:55 -0300

    test: add PersistenceTest suite

---
+++ backend/src/test/java/br/ifsp/demo/suites/SuitePersistenceTest.java
@@ -0,0 +1,13 @@
+package br.ifsp.demo.suites;
+
+import org.junit.platform.suite.api.IncludeTags;
+import org.junit.platform.suite.api.SelectPackages;
+import org.junit.platform.suite.api.Suite;
+import org.junit.platform.suite.api.SuiteDisplayName;
+
+@Suite
+@SelectPackages({"br.ifsp.demo.infrastructure.repository.donation"})
+@SuiteDisplayName("All persistence tests")
+@IncludeTags({"PersistenceTest"})
+public class SuitePersistenceTest {
+}
