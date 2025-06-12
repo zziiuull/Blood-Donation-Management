a58fedc0cec192d52f60182f973be201c663d987
Felipe Santos Lourenço <127883811+fezinnn@users.noreply.github.com>
2025-06-12 15:52:31 -0300

    test: add EntityBuilder and BaseApiIntegrationTest classes

---
+++ backend/src/test/java/br/ifsp/demo/presentation/controller/appointment/AppointmentControllerTest.java
@@ -0,0 +1,11 @@
+package br.ifsp.demo.presentation.controller.appointment;
+
+import br.ifsp.demo.presentation.security.user.User;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+
+class AppointmentControllerTest extends BaseApiIntegrationTest {
+    
+   
+}
