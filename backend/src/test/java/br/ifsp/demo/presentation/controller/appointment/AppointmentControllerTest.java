db416d8e2ff6dc4454437ed3f5c825c78696aa81
Felipe Santos Lourenço <127883811+fezinnn@users.noreply.github.com>
2025-06-12 23:08:08 -0300

    test: add shouldReturn401WhenAuthenticationFails test

--- backend/src/test/java/br/ifsp/demo/presentation/controller/appointment/AppointmentControllerTest.java
+++ backend/src/test/java/br/ifsp/demo/presentation/controller/appointment/AppointmentControllerTest.java
@@ -9,6 +9,7 @@ import br.ifsp.demo.domain.model.physician.Physician;
 import br.ifsp.demo.infrastructure.repository.appointment.AppointmentRepository;
 import br.ifsp.demo.infrastructure.repository.collectionSite.CollectionSiteRepository;
 import br.ifsp.demo.presentation.BaseApiIntegrationTest;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
@@ -20,7 +21,10 @@ import java.util.List;
 import java.util.Optional;
 
 import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.when;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.fail;
 
 
 class AppointmentControllerTest extends BaseApiIntegrationTest {
@@ -31,16 +35,20 @@ class AppointmentControllerTest extends BaseApiIntegrationTest {
     @Autowired
     private AppointmentRepository appointmentRepository;
 
+    private Physician user;
+    private String token;
+
+    @BeforeEach
+    void setUp() {
+        this.user = (Physician) registerPhysician("Teste123");
+        this.token = authenticate(user.getEmail(), "Teste123");
+    }
 
     @Test
     @Tag("ApiTest")
     @Tag("IntegrationTest")
     @DisplayName("Should return 200 and a list of appointments when they exist")
     void shouldReturn200AndAListOfAppointmentsWhenTheyExist(){
-        Physician user = (Physician) registerPhysician("Teste123");
-
-        String token = authenticate(user.getEmail(), "Teste123");
-
         CollectionSite site = collectionSiteRepository.save(new CollectionSite("Hemocentro",
                 new ContactInfo("email@email.com", "123", "address")));
 
@@ -57,7 +65,7 @@ class AppointmentControllerTest extends BaseApiIntegrationTest {
                 .when()
                 .get("/api/v1/appointment")
                 .then()
-                .statusCode(HttpStatus.OK.value())
+                .statusCode(200)
                 .extract()
                 .jsonPath().getList("", AppointmentDTO.class);
 
@@ -79,5 +87,12 @@ class AppointmentControllerTest extends BaseApiIntegrationTest {
         appointmentRepository.delete(appointment);
         repository.delete(user);
     }
-   
+
+    @Test
+    @DisplayName("Should return 401 when authentication fails")
+    void shouldReturn401WhenAuthenticationFails(){
+        given()
+        .when().get("/api/v1/appointment")
+        .then().statusCode(401);
+    }
 }
