63eac7e310b0bb70ba51d1d877871098427c3434
Felipe Santos Lourenço <127883811+fezinnn@users.noreply.github.com>
2025-06-19 16:05:39 -0300

    test: add RegisterPageObject class

---
+++ backend/src/test/java/br/ifsp/demo/ui/pageObject/RegisterPageObject.java
@@ -0,0 +1,8 @@
+package br.ifsp.demo.ui.pageObject;
+
+import org.openqa.selenium.WebDriver;
+
+public class RegisterPageObject extends BasePageObject{
+    public RegisterPageObject(WebDriver driver) { super(driver); }
+
+}
