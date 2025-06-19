1e3aa0633c1df12c6dbdcb48c35f783122635881
Felipe Santos Lourenço <127883811+fezinnn@users.noreply.github.com>
2025-06-19 18:58:29 -0300

    test: add setInitialPage method

--- backend/src/test/java/br/ifsp/demo/ui/pageTest/RegisterPageTest.java
+++ backend/src/test/java/br/ifsp/demo/ui/pageTest/RegisterPageTest.java
@@ -1,5 +1,10 @@
 package br.ifsp.demo.ui.pageTest;
 
 public class RegisterPageTest extends BaseSeleniumTest{
-    
+
+    @Override
+    protected void setInitialPage(){
+        String page = "http://localhost:3000/register";
+        driver.get(page);
+    }
 }
