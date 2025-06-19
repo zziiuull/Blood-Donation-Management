48144ce8945634a4c1aa8c479e58aaf06653dab3
Felipe Santos Lourenço <127883811+fezinnn@users.noreply.github.com>
2025-06-19 18:42:24 -0300

    test: add backToLogin method to RegisterPageObject - extract WebElements to variables

--- backend/src/test/java/br/ifsp/demo/ui/pageObject/RegisterPageObject.java
+++ backend/src/test/java/br/ifsp/demo/ui/pageObject/RegisterPageObject.java
@@ -2,29 +2,46 @@ package br.ifsp.demo.ui.pageObject;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
 
 public class RegisterPageObject extends BasePageObject{
     private static final String PAGE_TITLE = "Register";
+    private final WebElement nameInput = driver.findElement(By.id("name-input"));
+    private final WebElement lastNameInput = driver.findElement(By.id("lastname-input"));
+    private final WebElement cpfInput = driver.findElement(By.id("cpf-input"));
+    private final WebElement phoneInput = driver.findElement(By.id("phone-input"));
+    private final WebElement addressInput = driver.findElement(By.id("address-input"));
+    private final WebElement crmInput = driver.findElement(By.id("crm-input"));
+    private final WebElement emailInput = driver.findElement(By.id("email-input"));
+    private final WebElement passwordInput = driver.findElement(By.id("password-input"));
+    private final Select stateSelect = new Select(driver.findElement(By.id("react-aria2658155816-«r1f»")));
+    private final WebElement registerButton = driver.findElement(By.id("register-button"));
+    private final WebElement registerTitle = driver.findElement(By.id("register-title"));
+    private final WebElement loginButton = driver.findElement(By.id("login-link"));
 
     public RegisterPageObject(WebDriver driver) { super(driver); }
 
     public void register(String name, String lastName, String CPF, String phone,
-                         String address, String crm, String email, String password) {
-        driver.findElement(By.id("name-input")).sendKeys(name);
-        driver.findElement(By.id("lastname-input")).sendKeys(lastName);
-        driver.findElement(By.id("cpf-input")).sendKeys(CPF);
-        driver.findElement(By.id("phone-input")).sendKeys(phone);
-        driver.findElement(By.id("address-input")).sendKeys(address);
-        driver.findElement(By.id("crm-input")).sendKeys(crm);
-        driver.findElement(By.id("email-input")).sendKeys(email);
-        driver.findElement(By.id("password-input")).sendKeys(password);
+                         String address, String crm, int state, String email, String password) {
+        nameInput.sendKeys(name);
+        lastNameInput.sendKeys(lastName);
+        cpfInput.sendKeys(CPF);
+        phoneInput.sendKeys(phone);
+        addressInput.sendKeys(address);
+        crmInput.sendKeys(crm);
+        stateSelect.selectByIndex(state);
+        emailInput.sendKeys(email);
+        passwordInput.sendKeys(password);
 
-        final Select state = new Select(driver.findElement(By.id("react-aria2658155816-«r1f»")));
-        state.selectByVisibleText("Santa Catarina");
-
-        driver.findElement(By.id("register-button")).click();
+        registerButton.click();
     }
 
-    
+    public String getPageTitle(){
+        return registerTitle.getText();
+    }
+
+    public void backToLogin(){
+        loginButton.click();
+    }
 }
