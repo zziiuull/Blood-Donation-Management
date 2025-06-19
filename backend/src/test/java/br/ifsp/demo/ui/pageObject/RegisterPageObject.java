05909721de177ef72c357b9afbde5121c188df84
Felipe Santos Lourenço <127883811+fezinnn@users.noreply.github.com>
2025-06-19 18:16:22 -0300

    test: add register method on RegisterPageObject

--- backend/src/test/java/br/ifsp/demo/ui/pageObject/RegisterPageObject.java
+++ backend/src/test/java/br/ifsp/demo/ui/pageObject/RegisterPageObject.java
@@ -1,8 +1,30 @@
 package br.ifsp.demo.ui.pageObject;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.Select;
 
 public class RegisterPageObject extends BasePageObject{
+    private static final String PAGE_TITLE = "Register";
+
     public RegisterPageObject(WebDriver driver) { super(driver); }
 
+    public void register(String name, String lastName, String CPF, String phone,
+                         String address, String crm, String email, String password) {
+        driver.findElement(By.id("name-input")).sendKeys(name);
+        driver.findElement(By.id("lastname-input")).sendKeys(lastName);
+        driver.findElement(By.id("cpf-input")).sendKeys(CPF);
+        driver.findElement(By.id("phone-input")).sendKeys(phone);
+        driver.findElement(By.id("address-input")).sendKeys(address);
+        driver.findElement(By.id("crm-input")).sendKeys(crm);
+        driver.findElement(By.id("email-input")).sendKeys(email);
+        driver.findElement(By.id("password-input")).sendKeys(password);
+
+        final Select state = new Select(driver.findElement(By.id("react-aria2658155816-«r1f»")));
+        state.selectByVisibleText("Santa Catarina");
+
+        driver.findElement(By.id("register-button")).click();
+    }
+
+    
 }
