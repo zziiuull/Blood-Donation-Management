8aa8b12a5a16a6ebea985eeaa0888d158559412c
Felipe Santos Lourenço <127883811+fezinnn@users.noreply.github.com>
2025-06-18 22:51:23 -0300

    test: add shouldReturn400WhenExamDoesNotHaveCorrectStatusForThisDonationAnalysis test on DonationControllerTest

--- backend/src/test/java/br/ifsp/demo/presentation/controller/donation/DonationControllerTest.java
+++ backend/src/test/java/br/ifsp/demo/presentation/controller/donation/DonationControllerTest.java
@@ -399,5 +399,33 @@ class DonationControllerTest extends BaseApiIntegrationTest {
             examRepository.deleteById(immunoExam.getId());
             examRepository.deleteById(seroExam.getId());
         }
+
+        @Test
+        @Tag("ApiTest")
+        @Tag("IntegrationTest")
+        @DisplayName("Should return 400 when exam does not have correct status for this donation analysis")
+        void shouldReturn400WhenExamDoesNotHaveCorrectStatusForThisDonationAnalysis(){
+            Donation donation = donationRegisterService.registerByDonorId(elegibleDonor.getId(), appointment.getId());
+
+            ImmunohematologyExam immunoExam = new ImmunohematologyExam(donation);
+            immunoExam.setStatus(ExamStatus.APPROVED);
+            examRepository.save(immunoExam);
+
+            SerologicalScreeningExam seroExam = new SerologicalScreeningExam(donation);
+            seroExam.setStatus(ExamStatus.REJECTED);
+            examRepository.save(seroExam);
+
+            given()
+                    .header("Authorization", "Bearer " + token)
+                    .pathParam("id", donation.getId())
+                    .when()
+                    .put("/api/v1/donation/approve/{id}")
+                    .then()
+                    .statusCode(400);
+
+            donationRepository.deleteById(donation.getId());
+            examRepository.deleteById(immunoExam.getId());
+            examRepository.deleteById(seroExam.getId());
+        }
     }
 }
