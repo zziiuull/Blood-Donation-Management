d9aa273f5ddd7bcaffd29f1411d3590562faddcd
LucassSantoss <lucascross.pereirasantos@gmail.com>
2025-06-20 14:37:32 -0300

    refact(donationPageTest): create a setup method to register a user to login after it test: Should navigate to donation page when user login

--- backend/src/test/java/br/ifsp/demo/ui/pageTest/DonationPageTest.java
+++ backend/src/test/java/br/ifsp/demo/ui/pageTest/DonationPageTest.java
@@ -1,30 +1,89 @@
 package br.ifsp.demo.ui.pageTest;
 
+import br.ifsp.demo.presentation.security.auth.AuthenticationService;
+import br.ifsp.demo.presentation.security.auth.RegisterUserRequest;
+import br.ifsp.demo.presentation.security.user.JpaUserRepository;
+import br.ifsp.demo.ui.pageObject.AuthenticationPageObject;
 import br.ifsp.demo.ui.pageObject.DonationPageObject;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Tag;
-import org.junit.jupiter.api.Test;
+import com.github.javafaker.Faker;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.Duration;
+import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.fail;
 
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class DonationPageTest extends BaseSeleniumTest {
+    private static final Faker faker = new Faker();
+    private String email;
+    private String password;
+    private UUID userId;
+    private AuthenticationPageObject authPage;
+    private DonationPageObject donationPage;
+
+    @Autowired
+    JpaUserRepository userRepository;
+    @Autowired
+    AuthenticationService authenticationService;
+
     @Override
     protected void setInitialPage() {
-        String page = "http://localhost:3000/donation";
+        String page = "http://localhost:3000/login";
         driver.get(page);
     }
 
-    @Test
-    @Tag("UiTest")
-    @DisplayName("Should navigate to login if user is not logged in")
-    void shouldNavigateToLoginIfUserIsNotLoggedIn() {
-        new WebDriverWait(driver, Duration.ofSeconds(5))
-                .until(ExpectedConditions.urlContains("/login"));
-        assertThat(driver.getCurrentUrl()).contains("/login");
+    @Override
+    @BeforeEach
+    public void setUp() {
+        super.setUp();
+        authPage = new AuthenticationPageObject(driver);
+
+        email = faker.internet().emailAddress();
+        password = faker.internet().password(8, 16);
+
+        var request = new RegisterUserRequest(
+                faker.name().firstName(),
+                faker.name().lastName(),
+                email,
+                password
+        );
+
+        var registerResponse = authenticationService.register(request);
+        userId = registerResponse.id();
+    }
+
+    @AfterEach
+    public void tearDown() {
+        userRepository.deleteById(userId);
+    }
+
+    @Nested
+    class Navigate {
+        @Test
+        @Tag("UiTest")
+        @DisplayName("Should navigate to login if user is not logged in")
+        void shouldNavigateToLoginIfUserIsNotLoggedIn() {
+            ((JavascriptExecutor) driver).executeScript("window.localStorage.clear();");
+
+            new WebDriverWait(driver, Duration.ofSeconds(5))
+                    .until(ExpectedConditions.urlContains("/login"));
+            assertThat(driver.getCurrentUrl()).contains("/login");
+        }
+
+        @Test
+        @Tag("UiTest")
+        @DisplayName("Should navigate to donation page when user login")
+        void shouldNavigateToDonationPageWhenUserLogin(){
+            donationPage = authPage.authenticateWithCredentials(email, password);
+            new WebDriverWait(driver, Duration.ofSeconds(5)).until(ExpectedConditions.urlContains("/donation"));
+            assertThat(donationPage.pageUrl()).contains("/donation");
+        }
     }
 }
