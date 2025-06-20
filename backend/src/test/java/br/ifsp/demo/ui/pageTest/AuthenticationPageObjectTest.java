50b0a886d1638143d6e5a2eb60250c6e97399d57
Tiago Catoia <tiagocsz@hotmail.com>
2025-06-20 12:14:47 -0300

    test: add test to navigate to registrantion page by link on login page

--- backend/src/test/java/br/ifsp/demo/ui/pageTest/AuthenticationPageObjectTest.java
+++ backend/src/test/java/br/ifsp/demo/ui/pageTest/AuthenticationPageObjectTest.java
@@ -1,7 +1,70 @@
 package br.ifsp.demo.ui.pageTest;
 
-import static org.junit.jupiter.api.Assertions.*;
+import br.ifsp.demo.presentation.security.auth.AuthenticationService;
+import br.ifsp.demo.presentation.security.auth.RegisterUserRequest;
+import br.ifsp.demo.presentation.security.user.JpaUserRepository;
+import br.ifsp.demo.ui.pageObject.AuthenticationPageObject;
+import com.github.javafaker.Faker;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
-class AuthenticationPageObjectTest extends  BaseSeleniumTest{
+import java.time.Duration;
+import java.util.UUID;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class AuthenticationPageObjectTest extends BaseSeleniumTest {
+
+    private static final Faker faker = new Faker();
+    private String email;
+    private String password;
+    private UUID userId;
+
+    @Autowired
+    JpaUserRepository userRepository;
+    @Autowired
+    AuthenticationService authenticationService;
+
+    AuthenticationPageObject authPage;
+
+    @Override
+    protected void setInitialPage() {
+        driver.get("http://localhost:3000/login");
+    }
+
+    @BeforeEach
+    void setup() {
+        authPage = new AuthenticationPageObject(driver);
+
+        email = faker.internet().emailAddress();
+        password = faker.internet().password(8, 16);
+
+        var request = new RegisterUserRequest(
+                faker.name().firstName(),
+                faker.name().lastName(),
+                email,
+                password
+        );
+
+        var registerResponse = authenticationService.register(request);
+        userId = registerResponse.id();
+    }
+
+    @AfterEach
+    void cleanup() {
+        userRepository.deleteById(userId);
+    }
+
+    @Test
+    @Tag("UiTest")
+    @DisplayName("Should navigate to registration page by clicking the link")
+    void shouldNavigateToRegistrationPageByClickingTheLink() {
+        var registrationPage = authPage.navigateToRegistrationPage();
+        assertThat(registrationPage.currentUrl()).contains("/register");
+    }
 }
