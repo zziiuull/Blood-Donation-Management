ce18d6b2347bb0e32bfa71554b2bc2f5aa494d0f
Felipe Santos Lourenço <127883811+fezinnn@users.noreply.github.com>
2025-06-21 17:49:05 -0300

    test: add locators to UpdateImmunoExamPageObject

--- backend/src/test/java/br/ifsp/demo/ui/pageObject/UpdateImmunoExamPageObject.java
+++ backend/src/test/java/br/ifsp/demo/ui/pageObject/UpdateImmunoExamPageObject.java
@@ -1,10 +1,20 @@
 package br.ifsp.demo.ui.pageObject;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class UpdateImmunoExamPageObject extends BasePageObject{
+
+    private final By bloodTypeSelect = By.id("blood-type-select");
+    private final By irregularAntibodiesSelect = By.id("irregular-antibodies-select");
+    private final By observationsTextarea = By.id("observations-textarea");
+    private final By rejectButton = By.id("reject-button");
+    private final By approveButton = By.id("approve-button");
+
     public UpdateImmunoExamPageObject(WebDriver driver) {
         super(driver);
         if (!currentUrl().contains("/exams")) throw new IllegalStateException("Wrong page url: " + driver.getCurrentUrl());
     }
+
+
 }
