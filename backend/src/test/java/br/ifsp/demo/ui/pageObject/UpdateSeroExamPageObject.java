d7c7c7b9567c3a8538c7b07c712ce39485af9b30
Felipe Santos Lourenço <127883811+fezinnn@users.noreply.github.com>
2025-06-21 22:24:03 -0300

    test: add locators to UpdateSeroExamPageObject

--- backend/src/test/java/br/ifsp/demo/ui/pageObject/UpdateSeroExamPageObject.java
+++ backend/src/test/java/br/ifsp/demo/ui/pageObject/UpdateSeroExamPageObject.java
@@ -1,10 +1,24 @@
 package br.ifsp.demo.ui.pageObject;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class UpdateSeroExamPageObject extends BasePageObject {
+
+    private final By hepatitisBSelect = By.id("hepatitisb-select");
+    private final By hepatitisCSelect = By.id("hepatitisc-select");
+    private final By chagasDiseaseSelect = By.id("chagasDisease-select");
+    private final By syphilisSelect = By.id("syphilis-select");
+    private final By aidsSelect = By.id("aids-select");
+    private final By htlvSelect = By.id("htlv1_2-select");
+    private final By observationsTextarea = By.id("observations-textarea");
+    private final By rejectButton = By.id("reject-button");
+    private final By approveButton = By.id("approve-button");
+
     public UpdateSeroExamPageObject(WebDriver driver) {
         super(driver);
         if (!currentUrl().contains("/exams")) throw new IllegalStateException("Wrong page url: " + driver.getCurrentUrl());
     }
+
+
 }
